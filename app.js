04d188dca92863f90e84aee51356dfd4f1af5afc
THAKURkamlesh <kamlesh@wegile.com>
2025-07-04 13:05:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const port = 3000;
 
 http.createServer((req, res) => {
   res.end('🚀 Hello from Node.js test app!');
-}).listen(port, () => {
-  console.log(`🚀 Server running at http://localhost:${port}/`);
+}).listen(port, '0.0.0.0', () => {
+  console.log(`🚀 Server running at http://0.0.0.0:${port}/`);
 });
-
